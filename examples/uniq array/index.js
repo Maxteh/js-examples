3b7d92109beeca9c39d65a8a006c7e7fff8db54b
Maks Matson <acad.teh@gmail.com>
2021-03-16 01:08:09 +0500

    uniq values in array

---
+++ examples/uniq array/index.js
@@ -0,0 +1,52 @@
+/**
+ * Create function wich retrurn array with uniq values from given array
+ */
+
+const arrayToUniq = [1, 1, 2, 2, 4, 2, 3, 7, 3]
+
+/**
+ * First variant
+ */
+
+function uniqArray(arr) {
+  return [...new Set(arr)]
+}
+
+/**
+ * Second variant
+ */
+
+function uniqArraySecond(arr) {
+  const uniqArr = []
+
+  for (item of arr) {
+    if (uniqArr.indexOf(item) === -1) {
+      uniqArr.push(item)
+    }
+  }
+
+  return uniqArr
+}
+
+/**
+ * Third variant
+ */
+
+function uniqArrayThird(arr) {
+  const obj = {}
+
+  arr.forEach((item) => {
+    obj[item] = true
+  })
+  return Object.keys(obj).map((item) => +item)
+}
+
+/**
+ * Fourth variant
+ */
+
+function uniqArrayFourth(arr) {
+  return arr.filter((item, index, self) => {
+    return self.indexOf(item) === index
+  })
+}
